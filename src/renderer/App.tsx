5150d48ee1998f37f48216706d3fbc9d5a78065a
Róbert Darida <rdarida@users.noreply.github.com>
2025-11-12 18:17:28 +0100

    feat: configure CacheProvider

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,7 +1,9 @@
 import { StrictMode } from 'react';
 import { render } from 'react-dom';
-import { Provider } from 'react-redux';
 import { HashRouter, Route, Switch } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { CacheProvider } from '@emotion/react';
+import createCache from '@emotion/cache';
 import { CssBaseline, ThemeProvider, createTheme } from '@mui/material';
 
 import { store } from '@/store';
@@ -9,20 +11,23 @@ import { CounterView } from '@/views';
 
 import '@/styles/index.scss';
 
+const muiCache = createCache({ key: 'mui', prepend: true });
 const theme = createTheme();
 
 render(
   <StrictMode>
     <Provider store={store}>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
+      <CacheProvider value={muiCache}>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
 
-        <HashRouter basename="/main_window">
-          <Switch>
-            <Route exact path="/" component={CounterView} />
-          </Switch>
-        </HashRouter>
-      </ThemeProvider>
+          <HashRouter basename="/main_window">
+            <Switch>
+              <Route exact path="/" component={CounterView} />
+            </Switch>
+          </HashRouter>
+        </ThemeProvider>
+      </CacheProvider>
     </Provider>
   </StrictMode>,
   document.getElementById('root')
