0ac082ed752c59ec08d7c06b7a1c1639579a4282
Róbert Darida <rdarida@users.noreply.github.com>
2026-09-01 02:52:36 +0200

    feat: single instance

--- src/index.ts
+++ src/index.ts
@@ -10,78 +10,100 @@ import { productName } from '../package.json';
 
 if (started) {
   app.quit();
-}
+} else if (!app.requestSingleInstanceLock()) {
+  app.quit();
+} else {
+  let mainWindow: BrowserWindow | null = null;
 
-program
-  .name(productName)
-  .version(app.getVersion())
-  .option('-d, --devTools', 'Open DevTools')
-  .allowUnknownOption()
-  .parse(process.argv);
+  program
+    .name(productName)
+    .version(app.getVersion())
+    .option('-d, --devTools', 'Open DevTools')
+    .allowUnknownOption()
+    .parse(process.argv);
 
-function getIcon(): string | undefined {
-  const icon = join(app.getAppPath(), 'assets', 'icon');
+  const getIcon = (): string | undefined => {
+    const icon = join(app.getAppPath(), 'assets', 'icon');
 
-  switch (process.platform) {
-    case 'win32':
-      return icon + '.ico';
+    switch (process.platform) {
+      case 'win32':
+        return icon + '.ico';
 
-    case 'darwin':
-      return undefined;
+      case 'darwin':
+        return undefined;
 
-    default:
-      return icon + '.png';
-  }
-}
-
-const createWindow = () => {
-  const options = program.opts<CommandLineOptions>();
-
-  const mainWindow = new BrowserWindow({
-    title: `${productName} ${app.getVersion()}`,
-    icon: getIcon(),
-    minWidth: 1080,
-    minHeight: 720,
-    width: 1080,
-    height: 720,
-    show: false,
-    webPreferences: {
-      preload: join(__dirname, 'preload.js')
-    }
-  });
-
-  const show = (): void => {
-    mainWindow.show();
-
-    if (options.devTools) {
-      mainWindow.webContents.openDevTools({ mode: 'detach' });
+      default:
+        return icon + '.png';
     }
   };
 
-  if (MAIN_WINDOW_VITE_DEV_SERVER_URL) {
-    mainWindow.loadURL(MAIN_WINDOW_VITE_DEV_SERVER_URL).then(show);
-  } else {
-    mainWindow
-      .loadFile(
+  const createWindow = () => {
+    const options = program.opts<CommandLineOptions>();
+
+    mainWindow = new BrowserWindow({
+      title: `${productName} ${app.getVersion()}`,
+      icon: getIcon(),
+      minWidth: 1080,
+      minHeight: 720,
+      width: 1080,
+      height: 720,
+      show: false,
+      webPreferences: {
+        preload: join(__dirname, 'preload.js')
+      }
+    });
+
+    mainWindow.once('ready-to-show', () => {
+      mainWindow?.show();
+
+      if (options.devTools) {
+        mainWindow?.webContents.openDevTools({ mode: 'detach' });
+      }
+    });
+
+    mainWindow.on('closed', () => {
+      mainWindow = null;
+    });
+
+    if (MAIN_WINDOW_VITE_DEV_SERVER_URL) {
+      mainWindow.loadURL(MAIN_WINDOW_VITE_DEV_SERVER_URL);
+    } else {
+      mainWindow.loadFile(
         join(__dirname, `../renderer/${MAIN_WINDOW_VITE_NAME}/index.html`)
-      )
-      .then(show);
-  }
+      );
+    }
+  };
 
-  ipcMain.handle(Channel.COUNTER_INCREMENT, (e, value: number) => value + 1);
-  ipcMain.handle(Channel.COUNTER_DECREMENT, (e, value: number) => value - 1);
-};
+  app.on('ready', () => {
+    ipcMain.handle(Channel.COUNTER_INCREMENT, (e, value: number) => value + 1);
+    ipcMain.handle(Channel.COUNTER_DECREMENT, (e, value: number) => value - 1);
 
-app.on('ready', createWindow);
-
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
-});
-
-app.on('activate', () => {
-  if (BrowserWindow.getAllWindows().length === 0) {
     createWindow();
-  }
-});
+  });
+
+  app.on('second-instance', () => {
+    if (mainWindow) {
+      if (mainWindow.isMinimized()) {
+        mainWindow.restore();
+      }
+
+      if (!mainWindow.isVisible()) {
+        mainWindow.show();
+      }
+
+      mainWindow.focus();
+    }
+  });
+
+  app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+      app.quit();
+    }
+  });
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+}
